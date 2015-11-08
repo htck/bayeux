d0b288d2e0499c88751d72fc62c412eb4b029546
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-11-08 13:26:26 +0100

    Clearing sidenav related code

--- htck/app/scripts/controllers/main.js
+++ htck/app/scripts/controllers/main.js
@@ -299,38 +299,4 @@ angular.module('htckApp').controller('MainCtrl', function ($scope, $timeout, $lo
       }
 
       init();
-
-      // ---- Layout
-
-      /**
-     * Supplies a function that will continue to operate until the
-     * time is up.
-     */
-    function debounce(func, wait/*, context*/) {
-      var timer;
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     */
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug('toggle ' + navID + ' is done');
-          });
-      }, 200);
-    }
-    $scope.toggleLeft = buildDelayedToggler('left');
-
   });
