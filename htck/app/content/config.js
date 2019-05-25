846580d575cae91af15f7e6e19dd8905a304f242
Thomas Maurice <thomas@maurice.fr>
2019-05-25 12:27:44 +0100

    Fix the double slash problem

--- htck/app/content/config.js
+++ htck/app/content/config.js
@@ -2,22 +2,22 @@ var constants = {
   "config": {
     "appTitle": "Historic Tale Construction Kit",
     "appSub": "Bayeux",
-    "repository":"https://github.com/htck/bayeux",
+    "repository":"https:/github.com/htck/bayeux",
     "authors": [
       {
         "name": "LeonardA-L",
-        "picture": "https://leonarda-l.github.io/Photo_small.jpg",
-        "link": "https://LeonardA-L.github.io/"
+        "picture": "https:/leonarda-l.github.io/Photo_small.jpg",
+        "link": "https:/LeonardA-L.github.io/"
       },
       {
         "name": "Mathieu",
-        "picture": "https://secure.gravatar.com/avatar/e6cfb8715cecf121b64c938268519c50?s=64",
-        "link": "https://thoretton.com"
+        "picture": "https:/secure.gravatar.com/avatar/e6cfb8715cecf121b64c938268519c50?s=64",
+        "link": "https:/thoretton.com"
       },
       {
         "name": "Maria",
-        "picture": "https://avatars3.githubusercontent.com/u/6231239?v=3&s=460",
-        "link": "https://www.linkedin.com/in/metegan/"
+        "picture": "https:/avatars3.githubusercontent.com/u/6231239?v=3&s=460",
+        "link": "https:/www.linkedin.com/in/metegan/"
       }
     ]
   },
@@ -34,141 +34,141 @@ var constants = {
    {
       "title": "Braves",
       "images": [
-        "content//images/brave/01_archer1.png",
-        "content//images/brave/02_archer2.png",
-        "content//images/brave/03_archer3.png",
-        "content//images/brave/04_archer4.png",
-        "content//images/brave/05_falling_hero.png",
-        "content//images/brave/06_beheaded_warrior1.png",
-        "content//images/brave/07_dead_warrior2.png",
-        "content//images/brave/08_dead_warrior2.png",
-        "content//images/brave/09_dead_warrior3.png",
-        "content//images/brave/10_dead_warrior4.png",
-        "content//images/brave/11_horseman_with_shield_and_lance.png",
-        "content//images/brave/12_horsemen.png",
-        "content//images/brave/13_saluting_warrior.png",
-        "content//images/brave/14_two_warriors.png",
-        "content//images/brave/15_flag_carrying_warrior.png",
-        "content//images/brave/16_guy_in_shrubs.png",
-        "content//images/brave/17_driver_on_horse.png",
-        "content//images/brave/18_brave_with_axe.png",
+        "content/images/brave/01_archer1.png",
+        "content/images/brave/02_archer2.png",
+        "content/images/brave/03_archer3.png",
+        "content/images/brave/04_archer4.png",
+        "content/images/brave/05_falling_hero.png",
+        "content/images/brave/06_beheaded_warrior1.png",
+        "content/images/brave/07_dead_warrior2.png",
+        "content/images/brave/08_dead_warrior2.png",
+        "content/images/brave/09_dead_warrior3.png",
+        "content/images/brave/10_dead_warrior4.png",
+        "content/images/brave/11_horseman_with_shield_and_lance.png",
+        "content/images/brave/12_horsemen.png",
+        "content/images/brave/13_saluting_warrior.png",
+        "content/images/brave/14_two_warriors.png",
+        "content/images/brave/15_flag_carrying_warrior.png",
+        "content/images/brave/16_guy_in_shrubs.png",
+        "content/images/brave/17_driver_on_horse.png",
+        "content/images/brave/18_brave_with_axe.png",
       ]
     },
    {
       "title": "Beasts",
       "images": [
-        "content//images/beasts/01_horse.png",
-        "content//images/beasts/02_cow.png",
-        "content//images/beasts/03_bird1.png",
-        "content//images/beasts/04_bird2.png",
-        "content//images/beasts/05_bird3.png",
-        "content//images/beasts/06_bird4.png",
-        "content//images/beasts/07_bird5.png",
-        "content//images/beasts/08_bird6.png",
-        "content//images/beasts/09_ram1.png",
-        "content//images/beasts/10_otter.png",
-        "content//images/beasts/11_lion1.png",
-        "content//images/beasts/12_beast1.png",
-        "content//images/beasts/13_lion2.png",
-        "content//images/beasts/14_dog1.png",
-        "content//images/beasts/15_goat.png",
-        "content//images/beasts/16_dog2.png",
-        "content//images/beasts/17_dog3.png",
-        "content//images/beasts/18_dog4.png",
-        "content//images/beasts/19_beast2.png",
-        "content//images/beasts/20_dog5.png",
-        "content//images/beasts/21_dogs.png",
-        "content//images/beasts/22_lion3.png",
-        "content//images/beasts/23_lamb.png",
-        "content//images/beasts/24_donkey.png"
+        "content/images/beasts/01_horse.png",
+        "content/images/beasts/02_cow.png",
+        "content/images/beasts/03_bird1.png",
+        "content/images/beasts/04_bird2.png",
+        "content/images/beasts/05_bird3.png",
+        "content/images/beasts/06_bird4.png",
+        "content/images/beasts/07_bird5.png",
+        "content/images/beasts/08_bird6.png",
+        "content/images/beasts/09_ram1.png",
+        "content/images/beasts/10_otter.png",
+        "content/images/beasts/11_lion1.png",
+        "content/images/beasts/12_beast1.png",
+        "content/images/beasts/13_lion2.png",
+        "content/images/beasts/14_dog1.png",
+        "content/images/beasts/15_goat.png",
+        "content/images/beasts/16_dog2.png",
+        "content/images/beasts/17_dog3.png",
+        "content/images/beasts/18_dog4.png",
+        "content/images/beasts/19_beast2.png",
+        "content/images/beasts/20_dog5.png",
+        "content/images/beasts/21_dogs.png",
+        "content/images/beasts/22_lion3.png",
+        "content/images/beasts/23_lamb.png",
+        "content/images/beasts/24_donkey.png"
       ]
     },
    {
       "title": "Buildings",
       "images": [
-        "content//images/buildings/01_tower.png",
-        "content//images/buildings/02_tower2.png",
-        "content//images/buildings/03_tower3.png",
-        "content//images/buildings/04_tower4.png",
-        "content//images/buildings/05_fort.png",
-        "content//images/buildings/06_castle_on_hill.png",
-        "content//images/buildings/07_house.png",
-        "content//images/buildings/08_archway.png",
-        "content//images/buildings/09_pillars.png",
-        "content//images/buildings/10_pillar.png",
-        "content//images/buildings/11_tower6.png",
-        "content//images/buildings/12_tower5.png",
-        "content//images/buildings/13_Arch.png",
+        "content/images/buildings/01_tower.png",
+        "content/images/buildings/02_tower2.png",
+        "content/images/buildings/03_tower3.png",
+        "content/images/buildings/04_tower4.png",
+        "content/images/buildings/05_fort.png",
+        "content/images/buildings/06_castle_on_hill.png",
+        "content/images/buildings/07_house.png",
+        "content/images/buildings/08_archway.png",
+        "content/images/buildings/09_pillars.png",
+        "content/images/buildings/10_pillar.png",
+        "content/images/buildings/11_tower6.png",
+        "content/images/buildings/12_tower5.png",
+        "content/images/buildings/13_Arch.png",
       ]
     },
    {
       "title": "Folks",
       "images": [
-        "content//images/folc/01_group.png",
-        "content//images/folc/02_staring-guys.png",
-        "content//images/folc/03_shouting_guy.png",
-        "content//images/folc/04_pointing_guy.png",
-        "content//images/folc/05_gesturing_guy.png",
-        "content//images/folc/06_megaphone_guy.png",
-        "content//images/folc/07_guy_with_shield.png",
-        "content//images/folc/08_woman1.png",
-        "content//images/folc/09_cleric.png",
-        "content//images/folc/10_carrying_guy.png",
-        "content//images/folc/11_guy_with_ring.png",
-        "content//images/folc/12_guy_with_animal.png",
-        "content//images/folc/13_boy_with_sheep.png",
-        "content//images/folc/14_guy_with_axe.png",
-        "content//images/folc/15_pointing_guy.png",
-        "content//images/folc/16_fruitbasket_guy.png",
-        "content//images/folc/17_king1.png",
-        "content//images/folc/18_king2.png",
-        "content//images/folc/19_king3.png",
-        "content//images/folc/20_benchguys.png",
-        "content//images/folc/21_fireguy.png",
-        "content//images/folc/22_woman_with_child.png",
-        "content//images/folc/23_guy_with_chickens.png",
-        "content//images/folc/24_shouting_guy.png",
-        "content//images/folc/25_Man_pointing.png",
-        "content//images/folc/26_Serf.png",
-        "content//images/folc/27_SittingNoble01.png",
-        "content//images/folc/28_SittingNoble02.png",
-        "content//images/folc/29_SittingNoble03.png",
-        "content//images/folc/30_quasimodo.png",
-        "content//images/folc/31_people_fighting.png",
-        "content//images/folc/32_man_with_dog.png",
+        "content/images/folc/01_group.png",
+        "content/images/folc/02_staring-guys.png",
+        "content/images/folc/03_shouting_guy.png",
+        "content/images/folc/04_pointing_guy.png",
+        "content/images/folc/05_gesturing_guy.png",
+        "content/images/folc/06_megaphone_guy.png",
+        "content/images/folc/07_guy_with_shield.png",
+        "content/images/folc/08_woman1.png",
+        "content/images/folc/09_cleric.png",
+        "content/images/folc/10_carrying_guy.png",
+        "content/images/folc/11_guy_with_ring.png",
+        "content/images/folc/12_guy_with_animal.png",
+        "content/images/folc/13_boy_with_sheep.png",
+        "content/images/folc/14_guy_with_axe.png",
+        "content/images/folc/15_pointing_guy.png",
+        "content/images/folc/16_fruitbasket_guy.png",
+        "content/images/folc/17_king1.png",
+        "content/images/folc/18_king2.png",
+        "content/images/folc/19_king3.png",
+        "content/images/folc/20_benchguys.png",
+        "content/images/folc/21_fireguy.png",
+        "content/images/folc/22_woman_with_child.png",
+        "content/images/folc/23_guy_with_chickens.png",
+        "content/images/folc/24_shouting_guy.png",
+        "content/images/folc/25_Man_pointing.png",
+        "content/images/folc/26_Serf.png",
+        "content/images/folc/27_SittingNoble01.png",
+        "content/images/folc/28_SittingNoble02.png",
+        "content/images/folc/29_SittingNoble03.png",
+        "content/images/folc/30_quasimodo.png",
+        "content/images/folc/31_people_fighting.png",
+        "content/images/folc/32_man_with_dog.png",
       ]
     },
    {
       "title": "Misc",
       "images": [
-        "content//images/misc/01_tree.png",
-        "content//images/misc/02_tree.png",
-        "content//images/misc/03_tree.png",
-        "content//images/misc/04_tree.png",
-        "content//images/misc/05_tree.png",
-        "content//images/misc/06_boat.png",
-        "content//images/misc/07_boat.png",
-        "content//images/misc/07_boat_with_horses.png",
-        "content//images/misc/08_boat.png",
-        "content//images/misc/09_shield.png",
-        "content//images/misc/10_shield.png",
-        "content//images/misc/11_shields.png",
-        "content//images/misc/12_tree1.png",
-        "content//images/misc/13_tree.png",
-        "content//images/misc/14_guy_on_pole.png",
-        "content//images/misc/15_halleys_comet.png",
-        "content//images/misc/16_sword.png",
-        "content//images/misc/17_sword_helmet.png",
-        "content//images/misc/18_Barbecue.png",
+        "content/images/misc/01_tree.png",
+        "content/images/misc/02_tree.png",
+        "content/images/misc/03_tree.png",
+        "content/images/misc/04_tree.png",
+        "content/images/misc/05_tree.png",
+        "content/images/misc/06_boat.png",
+        "content/images/misc/07_boat.png",
+        "content/images/misc/07_boat_with_horses.png",
+        "content/images/misc/08_boat.png",
+        "content/images/misc/09_shield.png",
+        "content/images/misc/10_shield.png",
+        "content/images/misc/11_shields.png",
+        "content/images/misc/12_tree1.png",
+        "content/images/misc/13_tree.png",
+        "content/images/misc/14_guy_on_pole.png",
+        "content/images/misc/15_halleys_comet.png",
+        "content/images/misc/16_sword.png",
+        "content/images/misc/17_sword_helmet.png",
+        "content/images/misc/18_Barbecue.png",
       ]
     },
   ],
   "backgrounds":[
-    "content//images/backgrounds/background_1.jpg",
-    "content//images/backgrounds/background_2.jpg",
-    "content//images/backgrounds/background_3.jpg",
-    "content//images/backgrounds/background_4.jpg",
-    "content//images/backgrounds/background_5.jpg"
+    "content/images/backgrounds/background_1.jpg",
+    "content/images/backgrounds/background_2.jpg",
+    "content/images/backgrounds/background_3.jpg",
+    "content/images/backgrounds/background_4.jpg",
+    "content/images/backgrounds/background_5.jpg"
   ],
   "fonts":[
     {
@@ -185,7 +185,7 @@ var constants = {
   "brushes":[
     {
       name:"Folks",
-      icon:"content//images/folc/01_group.png",
+      icon:"content/images/folc/01_group.png",
       speed:100,
       randScaleRange: 0.1,
       randRotationRange: 3,
@@ -194,19 +194,19 @@ var constants = {
       randMirror: false,
       images:[
         {
-          img:"content//images/folc/01_group.png"
+          img:"content/images/folc/01_group.png"
         },
         {
-          img:"content//images/folc/02_staring-guys.png",
+          img:"content/images/folc/02_staring-guys.png",
           mirror:true,
           scale: 0.8
         },
         {
-          img:"content//images/folc/05_gesturing_guy.png",
+          img:"content/images/folc/05_gesturing_guy.png",
           scale:0.9
         },
         {
-          img:"content//images/folc/08_woman1.png",
+          img:"content/images/folc/08_woman1.png",
           mirror:true,
           scale: 0.9
         }
@@ -214,7 +214,7 @@ var constants = {
     },
     {
       name:"Folks-Mirror",
-      icon:"content//images/folc/02_staring-guys.png",
+      icon:"content/images/folc/02_staring-guys.png",
       speed:100,
       randScaleRange: 0.1,
       randRotationRange: 3,
@@ -223,19 +223,19 @@ var constants = {
       randMirror: false,
       images:[
         {
-          img:"content//images/folc/01_group.png"
+          img:"content/images/folc/01_group.png"
         },
         {
-          img:"content//images/folc/02_staring-guys.png",
+          img:"content/images/folc/02_staring-guys.png",
           mirror:true,
           scale: 0.8
         },
         {
-          img:"content//images/folc/05_gesturing_guy.png",
+          img:"content/images/folc/05_gesturing_guy.png",
           scale:0.9
         },
         {
-          img:"content//images/folc/08_woman1.png",
+          img:"content/images/folc/08_woman1.png",
           mirror:true,
           scale: 0.9
         }
@@ -243,7 +243,7 @@ var constants = {
     },
     {
       name:"Birds",
-      icon:"content//images/beasts/03_bird1.png",
+      icon:"content/images/beasts/03_bird1.png",
       speed:500,
       randScaleRange: 0.2,
       randRotationRange: 30,
@@ -251,20 +251,20 @@ var constants = {
       randMirror: true,
       images:[
         {
-          img:"content//images/beasts/03_bird1.png"
+          img:"content/images/beasts/03_bird1.png"
         },
         {
-          img:"content//images/beasts/05_bird3.png"
+          img:"content/images/beasts/05_bird3.png"
         },
         {
-          img:"content//images/beasts/08_bird6.png",
+          img:"content/images/beasts/08_bird6.png",
           scale:0.9
         }
       ]
     },
     {
       name:"Corpses",
-      icon:"content//images/brave/10_dead_warrior4.png",
+      icon:"content/images/brave/10_dead_warrior4.png",
       speed:450,
       randScaleRange: 0.2,
       randRotationRange: 10,
@@ -272,31 +272,31 @@ var constants = {
       randMirror: true,
       images:[
         {
-          img:"content//images/misc/16_sword.png",
+          img:"content/images/misc/16_sword.png",
           scale:0.8
         },
         {
-          img:"content//images/misc/17_sword_helmet.png",
+          img:"content/images/misc/17_sword_helmet.png",
           scale:0.7
         },
         {
-          img:"content//images/misc/09_shield.png",
+          img:"content/images/misc/09_shield.png",
           scale:0.7
         },
         {
-          img:"content//images/brave/06_beheaded_warrior1.png"
+          img:"content/images/brave/06_beheaded_warrior1.png"
         },
         {
-          img:"content//images/brave/07_dead_warrior2.png"
+          img:"content/images/brave/07_dead_warrior2.png"
         },
         {
-          img:"content//images/brave/08_dead_warrior2.png"
+          img:"content/images/brave/08_dead_warrior2.png"
         },
         {
-          img:"content//images/brave/09_dead_warrior3.png"
+          img:"content/images/brave/09_dead_warrior3.png"
         },
         {
-          img:"content//images/brave/10_dead_warrior4.png"
+          img:"content/images/brave/10_dead_warrior4.png"
         }
       ]
     }
